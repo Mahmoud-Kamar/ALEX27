ece244f5bb9ed8218ffca3681550bc90918a1b0d
Mahmoud Kamar <Mahmoud-Kamar>
2022-05-28 19:46:26 +0200

    first

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * File:   main.c
+ * Author: Mahmoud KaMar
+ *
+ * Created on May 14, 2022, 7:55 PM
+ */
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    /* Replace with your application code */
+    //set port as output
+    DDRA = 0xFF; //OR 0b11111111;// OR 255
+    // Set ALL Pins as HIGH
+    PORTA = 0b10101010; 
+    int x =0b11111111 ;
+    while (1) {
+        //if(PORTA == 0)
+         //   PORTA = 0x01;
+        _delay_ms(250);9
+        PORTA = (PORTA ^ x);
+    }
+}
